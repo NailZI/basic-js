2d34ab88332247c76c2cfc8d453fe0a7a4981b99
Nail <dubletrain76@gmai.com>
2022-04-29 15:42:05 +0300

    feat: mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,10 +23,59 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function minesweeper(matrix) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let result = [];
+
+  function check_mine(pos_i, pos_j) {
+    let sum = 0;
+    for (let x = (pos_i - 1); x < (pos_i + 2); x++) {
+      if ((x < 0) || (x === matrix.length)) {
+        // console.log('x=', x, 'y=')
+        continue;
+      } else {
+        for (let y = (pos_j - 1); y < (pos_j + 2); y++) {
+          if (y < 0  || (y === matrix[0].length)) {
+            // console.log('2: x=', x, 'y=', y)
+            continue;
+          } else if(matrix[x][y] === true) {
+            sum++;
+          } 
+        }
+      }
+    }
+    return sum;
+  }
+  
+  // check_mine(0, 1);
+  
+  
+  for (let i = 0; i < matrix.length; i++) {
+    let line_res = [];
+    for (let j = 0; j < matrix[0].length; j++) {
+      // console.log(i, j);
+      let sum = check_mine(i, j);
+      if (matrix[i][j] === true) {
+        sum--;
+      }
+      line_res.push(sum);
+      // console.log(line_res);
+    }
+    
+    result.push(line_res);
+    // console.log(result);
+  }
+  return result;
 }
+
+// matrix = [
+//   [true, false, false],
+//   [false, true, false],
+//   [false, false, false]
+//   ]
+
+// console.log(minesweeper(matrix));
 
 module.exports = {
   minesweeper
