a677908744a2b1a38dd0601f1fda7e56035638a9
Nail <dubletrain76@gmai.com>
2022-04-29 15:48:38 +0300

    fix: delete comments

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -24,20 +24,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]
  */
 function minesweeper(matrix) {
-  // throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
   let result = [];
-
   function check_mine(pos_i, pos_j) {
     let sum = 0;
     for (let x = (pos_i - 1); x < (pos_i + 2); x++) {
       if ((x < 0) || (x === matrix.length)) {
-        // console.log('x=', x, 'y=')
         continue;
       } else {
         for (let y = (pos_j - 1); y < (pos_j + 2); y++) {
           if (y < 0  || (y === matrix[0].length)) {
-            // console.log('2: x=', x, 'y=', y)
             continue;
           } else if(matrix[x][y] === true) {
             sum++;
@@ -47,24 +42,16 @@ function minesweeper(matrix) {
     }
     return sum;
   }
-  
-  // check_mine(0, 1);
-  
-  
   for (let i = 0; i < matrix.length; i++) {
     let line_res = [];
     for (let j = 0; j < matrix[0].length; j++) {
-      // console.log(i, j);
       let sum = check_mine(i, j);
       if (matrix[i][j] === true) {
         sum--;
       }
       line_res.push(sum);
-      // console.log(line_res);
     }
-    
     result.push(line_res);
-    // console.log(result);
   }
   return result;
 }
